cb2d4589953fa13095afe1fd61408baa074dfdf3
Alex <im@println.org>
2020-11-27 09:35:51 +0800

    rename plugin

--- src/main/java/intellij/plugin/StartupActivity.java
+++ src/main/java/intellij/plugin/StartupActivity.java
@@ -1,6 +1,7 @@
 package intellij.plugin;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
